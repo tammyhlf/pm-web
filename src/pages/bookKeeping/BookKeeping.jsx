161005947472d46ea6d585c75e1582cbee511b65
tammyhlf@icloud.com <540882>
2020-01-15 23:16:54 +0800

    update

--- src/pages/bookKeeping/BookKeeping.jsx
+++ src/pages/bookKeeping/BookKeeping.jsx
@@ -1,115 +1,127 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { Tabs, Icon, Row, Col } from 'antd';
 
 const { TabPane } = Tabs;
+const photo = {
+  eat: './eat.png'
+}
 
-export default () => (
-  <PageHeaderWrapper>
-    <div>
-      <Tabs defaultActiveKey="1" size="large">
-        <TabPane
-          tab={
-            <span>
-              <Icon type="account-book" />
-              支出
-            </span>
-          }
-          key="1"
-        >
-          <Row>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }} onClick={ () => this.handleClick() }>
-              <img src={ require('../../assets/pay-keeping/eat.png') } alt="eat"/>
-              <p>餐饮</p>
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-          </Row>
-        </TabPane>
-        <TabPane
-          tab={
-            <span>
-              <Icon type="dollar" />
-              收入
-            </span>
-          }
-          key="2"
-        >
-          <Row>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-            <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
-              Col
-            </Col>
-          </Row>
-        </TabPane>
-      </Tabs>
-    </div>
-  </PageHeaderWrapper>
-);
+class BookKeeping extends Component {
+  handleClick = () => {
+    console.log(1)
+  }
+  render() {
+    return (
+      <PageHeaderWrapper>
+        <div>
+          <Tabs defaultActiveKey="1" size="large">
+            <TabPane
+              tab={
+                <span>
+                  <Icon type="account-book" />
+                  支出
+                </span>
+              }
+              key="1"
+            >
+              <Row>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }} onClick={ () => this.handleClick() }>
+                  <img src={ photo.eat } alt="eat"/>
+                  <p>餐饮</p>
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane
+              tab={
+                <span>
+                  <Icon type="dollar" />
+                  收入
+                </span>
+              }
+              key="2"
+            >
+              <Row>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+                <Col xs={{ span: 6, offset: 2 }} lg={{ span: 3, offset: 1 }}>
+                  Col
+                </Col>
+              </Row>
+            </TabPane>
+          </Tabs>
+        </div>
+      </PageHeaderWrapper>
+    )
+  }
+}
+
+export default BookKeeping
